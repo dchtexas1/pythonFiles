205ae471c3efb671b0ca20ecd2744d02350611db
Dax Collin Henson <dchtexas1@gmail.com>
2017-02-13 12:49:47 -0600

    adventureText.py comments
    I commented adventureText; I might have cleaned some code I think. I
    didn't realize that it was due Monday @8a and not Wednesday, so I had to
    speed comment after class to turn it in late ASAP. I think I'll get a
    passable grade, especially since I did this all alone.

--- adventureText.py
+++ adventureText.py
@@ -1,8 +1,9 @@
 ###############################################################################
 # Name: Dax Henson
-# Date: 2017/02/1x
+# Date: 2017/02/12
 # Description: Adventure Text Game
 ###############################################################################
+# imports sleep for use in trap()
 from time import sleep
 ###############################################################################
 # the blueprint for a room
@@ -19,16 +20,19 @@ class Room(object):
         self.exitLocations = []
         self.items = []
         self.itemDescs = []
+        # hidden items are the same as items, but don't show up in room.__str__
         self.hiddenItems = []
         self.hiddenItemDescs = []
         self.grabbables = []
+        # grabbables have descriptions now
         self.grabbableDescs = []
+        # interactables are like items with binary state and require grabbables
         self.interactables = []
         self.interactableDescs = []
         self.interactableState = []
         self.interactableReqs = []
-    # getters and setters for the instance variables
 
+    # getters and setters for the instance variables
     @property
     def name(self):
         return self._name
@@ -130,6 +134,7 @@ class Room(object):
         return self._interactableReqs
 
     @interactableReqs.setter
+    # interactables are set to on by default
     def interactableReqs(self, value="on"):
         self._interactableReqs = value
 
@@ -141,9 +146,10 @@ class Room(object):
         self._exits.append(exit)
         self._exitLocations.append(room)
 
-    def delExit(self, exit, room):
-        self._exits.remove(exit)
-        self._exitLocations.remove(room)
+    # This is code I would like to implement in the future, for doors
+    # def delExit(self, exit, room):
+    #    self._exits.remove(exit)
+    #    self._exitLocations.remove(room)
 
     # adds an item to the room
     # the item is a string (e.g., table)
@@ -180,6 +186,7 @@ class Room(object):
     # returns a string description of the room
     def __str__(self):
         # first, the room name
+        # some room names require slightly different grammar
         if (currentRoom in [r0, r4, r5, r8, r9]):
             s = "You are {}.\n".format(self.name)
         else:
@@ -189,6 +196,7 @@ class Room(object):
         s += "You see: "
         for item in self.items:
             s += item + " "
+        # interactables are visible too
         for interactable in self.interactables:
             s += interactable + " "
         s += "\n"
@@ -204,9 +212,11 @@ class Room(object):
 ###############################################################################
 # creates the rooms
 def createRooms():
+    # some functions elsewhere need to check for these rooms
     global currentRoom, r0, r4, r5, r8, r9, r12
 
     # Generates all of the empty rooms
+    # all the rooms have names instead of "Room n"
     r1 = Room("an office")
     r2 = Room("a library")
     r3 = Room("a sunroom")
@@ -221,28 +231,31 @@ def createRooms():
     r12 = Room("a basement")
     r0 = Room("outside")
 
-    # Generates the office
-    r1.addExit("north", r1)
+    # generates the office
+    r1.addExit("north", r2)
     r1.addExit("east", r3)
+    # This keycard is used to change the state of the bomb
     r1.addGrabbable("keycard", "It is used to prove one's credentials.")
     r1.addItem("desk", "A modern desk, made of glass and carbon-fiber. "
                "There is a keycard on the desk\nbeside an inlaid terminal.")
     r1.addItem("chair", "It is smooth, black, and shiny.")
     r1.addItem("terminal", "The terminal glows an amber color. "
                "There is text on the screen.")
+    # this text can be looked, but it doesn't show up in Room.__str__
     r1.addHiddenItem("text", "The terminal displays a draft of a message to "
                      "the separatist chancellor about\ntransporting a bomb "
                      "out of the basement.")
 
-    # Generates the library
+    # generates the library
     r2.addExit("south", r1)
     r2.addItem("rug", "It is awfully plush.")
-    r2.addItem("fireplace", "It is warm, but dying.")
     r2.addItem("bookshelves", "The bookshelves are neatly packed with books.\n"
-               "One of the books is not flush with the others.")
+               "One of the books is not flush with the others. It appears to "
+               "be a journal.")
+    # alas, I am a programmer, not an author
     r2.addGrabbable("journal", "\"Some poignant world-building.\"")
 
-    # Generates room 3
+    # Generates a sunroom
     r3.addExit("north", r4)
     r3.addExit("west", r1)
     r3.addExit("south", None)
@@ -250,45 +263,62 @@ def createRooms():
                "because of the sieg heil pose.")
     r3.addItem("couch", "Rather plain, given the ornate statue nearby.")
 
+    # generates a stairwell room
     r4.addExit("south", r3)
+    # here we have "vertical movement" though technically there's no dimensions
     r4.addExit("down", r5)
     r4.addItem("stairwell", "There is some light downstairs, and an..."
                "interesting smell wafts through.")
 
+    # generates a stairwell room
     r5.addExit("up", r4)
     r5.addExit("west", r6)
-    r5.addItem("stairwell", "It's rather dark upstairs, now that your eyes"
+    r5.addItem("stairwell", "It's rather dark upstairs, now that your eyes "
                "have adjusted.")
 
+    # generates a sitting room, with access to the outside
     r6.addExit("east", r5)
     r6.addExit("south", r7)
     r6.addExit("west", r0)
+    r6.addItem("fireplace", "It is warm, but dying.")
+    r6.addItem("coffee table", "It is littered with decorative pieces.")
 
+    # generates a kitchen
     r7.addExit("north", r6)
     r7.addExit("east", r8)
+    r7.addItem("oven", "There is an apple pie warming in it.")
 
+    # generates another stairwell room
     r8.addExit("west", r7)
     r8.addExit("down", r9)
+    r8.addItem("stairwell", "This stairwell is extremely deep.")
 
+    # generates basement stairwell room
     r9.addExit("up", r8)
     r9.addExit("west", r10)
+    r9.addExit("stairwell", "Returning to the surface will surely exhausting.")
 
+    # generates extra basement room
     r10.addExit("east", r9)
     r10.addExit("north", r11)
 
+    # generates extra basement room
     r11.addExit("south", r10)
     r11.addExit("east", r12)
 
+    # generates a basement room with an interactive bomb requiring a keycard
     r12.addExit("west", r11)
     r12.addInteractable("bomb", "It must be deactivated to save the Alliance. "
                         "There is a card slot on the side.", "on", "keycard")
 
+    # generates an room "outside." Of course, it's only outside bc I say so
     r0.addExit("east", r6)
     r0.addItem("trees", "The tree is devoid of leaves. This would lead you "
                "to assume that it is winter,\nexcept for the fact that it is "
                "hot enough to fry an egg.")
     r0.addItem("grass", "The grass is brown and dead.")
 
+    # sets which room the user starts in
     currentRoom = r1
 
 
@@ -302,7 +332,7 @@ def death():
     print " " * 8 + "u" + "$" * 23 + "u"
     print " " * 7 + "u" + "$" * 25 + "u"
     print " " * 7 + "u" + "$" * 25 + "u"
-    print " " * 7 + "u" + "$" * 6 + "\"" + " " * 3 + "\"" + "$" * 3 + "\"" " " * 3 + "\"" + "$" * 6 + "u"
+    print " " * 7 + "u" + "$" * 6 + "\"" + " " * 3 + "\"" + "$" * 3 + "\"" + " " * 3 + "\"" + "$" * 6 + "u"
     print " " * 7 + "\"" + "$" * 4 + "\"" + " " * 6 + "u$u" + " " * 7 + "$" * 4 + "\""
     print " " * 8 + "$" * 3 + "u" + " " * 7 + "u$u" + " " * 7 + "u" + "$" * 3
     print " " * 8 + "$" * 3 + "u" + " " * 6 + "u" + "$" * 3 + "u" + " " * 6 + "u" + "$" * 3
@@ -323,6 +353,7 @@ def death():
     print " " * 5 + "$" * 3 + "\"" + " " * 25 + "$" * 4 + "\""
 
 
+# this is a slightly different way to die; still leads to death()
 def trap():
     sleep(5)
     response = "The bomb starts ticking! Whoops!"
@@ -335,7 +366,9 @@ def trap():
 # START THE GAME!!!
 createRooms()
 inventory = []
+# lets the inventory be looked, no matter which room you are in
 inventoryDesc = []
+# allows the user to input the first letter instead of the full direction
 directions = ["north", "south", "east", "west", "up", "down"]
 shortcuts = ["n", "s", "e", "w", "u", "d"]
 
@@ -344,12 +377,11 @@ while True:
         death()
         break
 
+    # checks if the bomb state has been altered, thus killing you
     if (currentRoom == r12):
         if (currentRoom.interactableState[[i for i, j in enumerate(currentRoom.interactables) if j == 'bomb'][0]] == "off"):
             trap()
             break
-
-    # if ("keycard" in inventory):
 
     status = "{}\nYou are carrying: {}\n".format(currentRoom, inventory)
 
@@ -360,12 +392,13 @@ while True:
     action = raw_input("What do you want to do? ")
     action = action.lower()
 
+    # allows the user to send blank input without a response being printed
     while (action == ""):
         action = raw_input("")
-        action = action.lower()
     else:
         response = "I don't understand. Try valid noun.\n"\
-            "Valid verbs are:\n\n[go, head]\n[look, check]\n[take, get]"
+            "Valid verbs are:\n\nMovement: [go, head]\nSight:    [look, check]\n"\
+            "Action:   [take, get] [use]"
 
     if (action in ["quit", "exit", "bye"]):
         break
@@ -378,6 +411,7 @@ while True:
         if (verb in ["go", "head"]):
             response = "Invalid exit"
 
+            # expands shortcuts to the full direction for parsing
             if (noun in shortcuts):
                 noun = directions[shortcuts.index(noun)]
 
@@ -397,21 +431,25 @@ while True:
                     response = currentRoom.itemDescs[i]
                     break
 
+            # allows interactables to be looked
             for i in range(len(currentRoom.interactables)):
                 if (noun == currentRoom.interactables[i]):
                     response = currentRoom.interactableDescs[i]
                     break
 
+            # allows hiddenItems to be looked, despite that they're not seen
             for i in range(len(currentRoom.hiddenItems)):
                 if (noun == currentRoom.hiddenItems[i]):
                     response = currentRoom.hiddenItemDescs[i]
                     break
 
+            # allows un-taken grabbables to be looked
             for i in range(len(currentRoom.grabbables)):
                 if (noun == currentRoom.grabbables[i]):
                     response = currentRoom.grabbableDescs[i]
                     break
 
+            # allows taken grabbables to be looked
             for i in range(len(inventory)):
                 if (noun == inventory[i]):
                     response = inventoryDesc[i]
