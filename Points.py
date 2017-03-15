b36242464ac727e5883c2c6a5fbd7b0dfedefe48
Dax Collin Henson <dchtexas1@gmail.com>
2017-03-15 04:29:41 -0500

    Points.py
    Created and worked on the first 132 assignment. Slightly ahead of time.
    I only need to add the distance and midpoint functions, I think.

---
+++ Points.py
@@ -0,0 +1,58 @@
+###############################################################################
+# Name: Dax Henson
+# Date: 2017/03/1x
+# Description: Implements 2D point class in Python.
+###############################################################################
+
+
+# the 2D point class
+class Point(object):
+
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, arg):
+        self._x = float(arg)
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, arg):
+        self._y = float(arg)
+
+    def dist(self, arg):
+        pass
+
+    def midpt(self, arg):
+        pass
+
+    def __str__(self):
+        return("({},{})").format(self.x, self.y)
+
+
+##########################################################
+# ***DO NOT MODIFY OR REMOVE ANYTHING BELOW THIS POINT!***
+# create some points
+p1 = Point()
+p2 = Point(3, 0)
+p3 = Point(3, 4)
+# display them
+print "p1:", p1
+print "p2:", p2
+print "p3:", p3
+# calculate and display some distances
+print "distance from p1 to p2:", p1.dist(p2)
+print "distance from p2 to p3:", p2.dist(p3)
+print "distance from p1 to p3:", p1.dist(p3)
+# calculate and display some midpoints
+print "midpt of p1 and p2:", p1.midpt(p2)
+print "midpt of p2 and p3:", p2.midpt(p3)
+print "midpt of p1 and p3:", p1.midpt(p3)
