59a3c516618cc65f3184b05f150433db5e9efb61
Dax Collin Henson <dchtexas1@gmail.com>
2017-04-28 10:39:05 -0500

    Chaos? and Paper Piano I did something with Chaos.py; and made a piano that plays middle C in four waveforms

--- Chaos.py
+++ Chaos.py
@@ -70,24 +70,27 @@ class ChaosGame(Canvas):
     def vertices(self):
         # the top vertex
         v1 = Point(WIDTH / 2, 0)
-        self.plot(v1.x, v1.y, VERTEX_COLOR, "vertex")
+        self.plot(v1.x, v1.y, "red", "vertex")
         # the bottom left vertex
         v2 = Point(0, HEIGHT)
-        self.plot(v2.x, v2.y, VERTEX_COLOR, "vertex")
+        self.plot(v2.x, v2.y, "green", "vertex")
         # the bottom right vertex
         v3 = Point(WIDTH, HEIGHT)
-        self.plot(v3.x, v2.y, VERTEX_COLOR, "vertex")
-        # creates an array to keep track of the list
+        self.plot(v3.x, v2.y, "blue", "vertex")
+        # creates an array to keep track of the vertices
         global vtcs
         vtcs = [v1, v2, v3]
 
     def plot(self, x, y, color, pointType="point"):
         if (x < WIDTH / 2 and y > HEIGHT / 2):
-            color = "green"
+            # green
+            color = "#%02x%02x%02x" % (0, 255, 0)
         elif (x > WIDTH / 2 and y > HEIGHT / 2):
-            color = "blue"
+            # blue
+            color = "#%02x%02x%02x" % (0, 0, 255)
         elif (y < HEIGHT / 2):
-            color = "red"
+            # red
+            color = "#%02x%02x%02x" % (255, 0, 0)
         if (pointType == "vertex"):
             self.create_oval(x, y, x + VERTEX_RADIUS * 2,
                              y + VERTEX_RADIUS * 2, outline=color, fill=color)
